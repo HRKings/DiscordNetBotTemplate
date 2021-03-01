aa807a5bd17c3a5bae36ae27822c6f18f6e35e9f
Helton Reis <hrkings@rhaizzing.com>
2021-03-01 18:33:51 -0300

    [misc] Initial commit

---
+++ DiscordNet/Modules/HelpModule.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordNet.Modules
+{
+    public class HelpHandler : ModuleBase<SocketCommandContext>
+    {
+        private readonly CommandService _service;
+
+        public HelpHandler(CommandService service)
+        {
+            _service = service;
+        }
+
+        [Command("Help")]
+        [Summary("Lists all commands")]
+        public async Task HelpAsync()
+        {
+            List<CommandInfo> commands = _service.Commands.ToList();
+            EmbedBuilder embedBuilder = new EmbedBuilder();
+
+            foreach (CommandInfo command in commands)
+            {
+                // Get the command Summary attribute information
+                string embedFieldText = command.Summary ?? "No description available\n";
+
+                embedBuilder.AddField(command.Name, embedFieldText);
+            }
+
+            await ReplyAsync("Here's a list of commands and their description: ", false, embedBuilder.Build());
+        }
+    }
+}
