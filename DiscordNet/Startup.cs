e2796256056f8588ee10faf80cc47049db16ab70
Helton Reis <hrkings@rhaizzing.com>
2021-06-04 21:45:22 -0300

    [chore] Update gitignore

--- DiscordNet/Startup.cs
+++ DiscordNet/Startup.cs
@@ -31,19 +31,7 @@ namespace DiscordNet
             _client.Log += baseLog;
         }
 
-        private Task OnReady()
-        {
-            // Prints to the console all the servers the bot is connected to
-            Console.WriteLine($"Connected to these servers as '{_client.CurrentUser.Username}': ");
-
-            foreach (var guild in _client.Guilds) Console.WriteLine($"- {guild.Name}");
-
-            // Choose between the activities and change to the chosen one
-            _client.SetGameAsync("and Testing");
-            Console.WriteLine($"Activity set to 'Playing {_client.Activity.Name}'");
-
-            return Task.CompletedTask;
-        }
+       
 
         public IServiceProvider BuildServiceProvider()
         {
